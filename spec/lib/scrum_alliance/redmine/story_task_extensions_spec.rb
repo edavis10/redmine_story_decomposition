dbbbf9e59dfa856f47b4a3c7a75ee38a199079f6
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-10 11:17:21 -0700

    Added specs for the StoryTaskExtensions to Issue. #2081

--- spec/lib/scrum_alliance/redmine/story_task_extensions_spec.rb
+++ spec/lib/scrum_alliance/redmine/story_task_extensions_spec.rb
@@ -18,11 +18,40 @@ describe Issue, 'story?' do
 end
 
 describe Issue, 'story' do
-  it 'should return the parent story for an issue'
-  it 'should return nothing if the issue is not a story'
+  it 'should return the parent story for an issue' do
+    parent_issue = mock_model(Issue)
+    child_issue = Issue.new
+    child_issue.should_receive(:relations_to).and_return do
+      relation = mock_model(IssueRelation)
+      relation.should_receive(:relation_type).and_return('composes')
+      relation.should_receive(:issue_from).and_return(parent_issue)
+      
+      [relation]
+    end
+
+    child_issue.story.should eql(parent_issue)
+  end
 end
 
 describe Issue, 'tasks' do
-  it 'should return the child tasks for a story issue'
-  it 'should return nothing if the issue is not a story'
+  it 'should return the child tasks for a story issue' do
+    child_issue_one = mock_model(Issue)
+    child_issue_two = mock_model(Issue)
+    child_issue_three = mock_model(Issue)
+    children = [child_issue_one, child_issue_two, child_issue_three]
+    parent_issue = Issue.new
+    parent_issue.should_receive(:relations_from).and_return do
+      relations = []
+      children.each do |child_issue|
+        relation = mock_model(IssueRelation)
+        relation.should_receive(:relation_type).and_return('composes')
+        relation.should_receive(:issue_to).and_return(child_issue)
+        relations << relation
+      end
+      relations
+    end
+
+    parent_issue.tasks.should eql(children)
+    
+  end
 end
